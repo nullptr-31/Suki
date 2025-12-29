c1d8209f4158ed285a1b822f4d4d95e1446aa90f
ABDELBAKI Boukerche <boukerche.abdelbaki@gmail.com>
2025-12-29 23:33:04 +0100

    Finished Event system and added glm

--- engine/src/Platform/NativeWindow/NativeWindow.cpp
+++ engine/src/Platform/NativeWindow/NativeWindow.cpp
@@ -1,9 +1,12 @@
 #include "Platform/NativeWindow/NativeWindow.h"
 
+#include "GLFW/glfw3.h"
 #include "Platform/OpenGL/OpenGLContext.h"
 #include "Suki/Core/Assert.h"
 #include "Suki/Core/Logger.h"
 #include "Suki/Event/ApplicationEvent.h"
+#include "Suki/Event/KeyEvent.h"
+#include "Suki/Event/MouseEvent.h"
 
 namespace Suki
 {
@@ -63,6 +66,85 @@ void NativeWindow::Init(const WindowSpecs& specs)
             WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
 
             WindowCloseEvent event;
+            data.EventCallback(event);
+        });
+
+    glfwSetKeyCallback(
+        m_Window,
+        [](GLFWwindow* window, int key, int scancode, int action, int mods)
+        {
+            WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+            switch(action)
+            {
+                case GLFW_PRESS: {
+                    KeyPressedEvent event(key, 0);
+                    data.EventCallback(event);
+                    break;
+                }
+                case GLFW_RELEASE: {
+                    KeyReleasedEvent event(key);
+                    data.EventCallback(event);
+                    break;
+                }
+                case GLFW_REPEAT: {
+                    KeyPressedEvent event(key, true);
+                    data.EventCallback(event);
+                    break;
+                }
+            }
+        });
+
+    glfwSetCharCallback(m_Window,
+                        [](GLFWwindow* window, unsigned int keycode)
+                        {
+                            WindowData& data =
+                                *(WindowData*)glfwGetWindowUserPointer(window);
+
+                            KeyTypedEvent event(keycode);
+                            data.EventCallback(event);
+                        });
+
+    glfwSetMouseButtonCallback(
+        m_Window,
+        [](GLFWwindow* window, int button, int action, int mods)
+        {
+            WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+            switch(action)
+            {
+                case GLFW_PRESS: {
+                    MouseButtonPressedEvent event(button);
+                    data.EventCallback(event);
+
+                    break;
+                }
+                case GLFW_RELEASE: {
+                    MouseButtonReleasedEvent event(button);
+                    data.EventCallback(event);
+
+                    break;
+                }
+            }
+        });
+
+    glfwSetScrollCallback(
+        m_Window,
+        [](GLFWwindow* window, double xOffset, double yOffset)
+        {
+            WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+            MouseScrolledEvent event((float)xOffset, (float)yOffset);
+            data.EventCallback(event);
+        });
+
+    glfwSetCursorPosCallback(
+        m_Window,
+        [](GLFWwindow* window, double xPos, double yPos)
+        {
+            WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+            MouseMovedEvent event((float)xPos, (float)yPos);
             data.EventCallback(event);
         });
 }
