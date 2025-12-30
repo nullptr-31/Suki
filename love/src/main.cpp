359dff63379aea52e6fe1ae0ee4925ab8ee089f5
ABDELBAKI Boukerche <boukerche.abdelbaki@gmail.com>
2025-12-30 13:18:05 +0100

    Changed Specs

--- love/src/main.cpp
+++ love/src/main.cpp
@@ -7,9 +7,6 @@ int main()
     Suki::Logger::Init();
 
     Suki::ApplicationSpecs specs;
-    specs.Name = "LOVE --The Game--";
-    specs.Author = "Soumia & Abdelbaki";
-
     Suki::Application* app = new Suki::Application(specs);
 
     app->Run();
